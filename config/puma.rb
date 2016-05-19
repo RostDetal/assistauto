35a2722e770049e73728b8f4a1820f2b962f696f
Igor Vaganov <i.vaganov@socialquantum.ru>
2016-05-19 13:22:10 +0300

    Deploy t.v4

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,26 @@
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}"
 
-directory app_dir
+
 
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 if rails_env!="development"
-  pidfile "#{shared_dir}/tmp/pids/puma.pid"
-  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-  state_path "#{shared_dir}/tmp/pids/puma.state"
-  activate_control_app "#{shared_dir}/tmp/sockets/pumactl.sock"
-
   threads 2, 16
   workers 4
 
-  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
   daemonize true
+  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+  pidfile "#{shared_dir}/tmp/pids/puma.pid"
+  state_path "#{shared_dir}/tmp/pids/puma.state"
+  activate_control_app "#{shared_dir}/tmp/sockets/pumactl.sock"
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+  preload_app!
+
+
+
+
+
 end
