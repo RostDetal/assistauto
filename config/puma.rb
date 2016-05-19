a7727c6dfd8152d19f748b0a67200504c2f118ec
Igor Vaganov <i.vaganov@socialquantum.ru>
2016-05-19 11:25:04 +0300

    Deploy test

--- config/puma.rb
+++ config/puma.rb
@@ -9,9 +9,9 @@ if rails_env!="development"
   stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
   state_path "#{shared_dir}/tmp/pids/puma.state"
   activate_control_app "#{shared_dir}/tmp/sockets/pumactl.sock"
-
-  threads 2, 16
-  workers 4
+  #
+  # threads 2, 16
+  # workers 4
 
   bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
   # daemonize true
