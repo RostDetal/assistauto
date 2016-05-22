b3d0f2d6af00c49e06fbaa23f7140fbe08e88cf3
V.i.S <i@asistes.com>
2016-05-22 10:04:30 -0700

    Content type spoof 1

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,11 +5,7 @@ module Paperclip
     old_spoofed = instance_method(:spoofed?)
 
     define_method(:spoofed?) do
-      if supplied_file_content_types.count > 0
-        old_spoofed.bind(self).()
-      else
         false
-      end
     end
   end
 end
