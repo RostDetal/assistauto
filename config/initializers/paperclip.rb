eb1d4b688bdd67a4ec01ad16fa8f01272430c1ad
V.i.S <i@asistes.com>
2016-05-21 22:37:42 -0700

    Content type spoof

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,15 +1,3 @@
-module Paperclip
-  class MediaTypeSpoofDetector
-    def type_from_file_command
-      begin
-        Paperclip.run("file", "-b --mime :file", :file => @file.path)
-      rescue Cocaine::CommandLineError
-        ""
-      end
-    end
-  end
-end
-
 Paperclip.options[:content_type_mappings] = {
     :jpg => "image/jpeg",
     :jpeg => "image/jpeg",
