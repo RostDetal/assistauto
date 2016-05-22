e340efd552ca6b68f412458e361e2e12c54edbfa
V.i.S <i@asistes.com>
2016-05-22 09:19:39 -0700

    Content type spoof

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,15 @@
-Paperclip.options[:command_path] = '/usr/bin'
-Paperclip.options[:content_type_mappings] = {
-    :jpg => "image/jpeg",
-    :jpeg => "image/jpeg",
-    :png => "image/png",
-    :gif => "image/gif",
-}
+require 'paperclip/media_type_spoof_detector'
+
+module Paperclip
+  class MediaTypeSpoofDetector
+    old_spoofed = instance_method(:spoofed?)
+
+    define_method(:spoofed?) do
+      if supplied_file_content_types.count > 0
+        old_spoofed.bind(self).()
+      else
+        false
+      end
+    end
+  end
+end
